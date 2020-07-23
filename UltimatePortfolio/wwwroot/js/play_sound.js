9a144cc99aea1a9fe9ce763ae31af5e89c66e692
getrajer533 <getrajer533@gmail.com>
2020-07-23 13:22:19 +0100

    Add project files.

---
+++ UltimatePortfolio/wwwroot/js/play_sound.js
@@ -0,0 +1,11 @@
+﻿function PlayUISound() {
+    var audio = document.getElementsByTagName("audio")[0];
+    audio.play();
+}
+
+
+function StopUISound() {
+    var audio = document.getElementsByTagName("audio")[0];
+    thissound.pause();
+    thissound.currentTime = 0;
+}
